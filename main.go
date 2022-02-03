c57208f3ffd53b60529d4dae72785be75c1f3f1a
daisuke8000 <daisuke12108000@gmail.com>
2022-02-04 01:58:28 +0900

    oauth requestcode

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 const (
 	scope     = "https://www.googleapis.com/auth/photoslibrary.readonly"
 	grantType = "authorization_code"
+	verifier  = "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"
 )
 
 var secret map[string]interface{}
 
 var oauth struct {
-	clientId          string
-	clientSecret      string
-	authEndPoint      string
-	tokenEndPoint     string
-	clientRedirectUri string
-	scope             string
-	state             string
-	responseType      string
+	clientId            string
+	clientSecret        string
+	authEndPoint        string
+	tokenEndPoint       string
+	clientRedirectUri   string
+	codeChallengeMethod string
+	codeChallenge       string
+	scope               string
+	state               string
+	responseType        string
 }
 
 func readJson() {
-	data, err := ioutil.ReadFile("/client_secret.json")
+	data, err := ioutil.ReadFile("./client_secret.json")
 
 	if err != nil {
 		log.Fatal(err)
@@ -41,13 +48,21 @@ func setUp() {
 
 	readJson()
 
-	oauth.clientId = secret["web"].(map[string]interface{})["clientId"].(string)
-	oauth.clientSecret = secret["web"].(map[string]interface{})["clientSecret"].(string)
-	oauth.authEndPoint = secret["web"].(map[string]interface{})["authEndPoint"].(string)
-	oauth.tokenEndPoint = secret["web"].(map[string]interface{})["tokenEndPoint"].(string)
+	oauth.clientId = secret["web"].(map[string]interface{})["client_id"].(string)
+	oauth.clientSecret = secret["web"].(map[string]interface{})["client_secret"].(string)
+	oauth.authEndPoint = secret["web"].(map[string]interface{})["auth_uri"].(string)
+	oauth.tokenEndPoint = secret["web"].(map[string]interface{})["token_uri"].(string)
+	oauth.clientRedirectUri = secret["web"].(map[string]interface{})["redirect_uris"].([]interface{})[0].(string)
 	oauth.scope = scope
 	oauth.state = "xyz"
-	oauth.responseType = "code"
+	oauth.responseType = "token"
+	oauth.codeChallengeMethod = "S256"
+	oauth.codeChallenge = base64URLEncode()
+}
+
+func base64URLEncode() string {
+	hash := sha256.Sum256([]byte(verifier))
+	return base64.RawURLEncoding.EncodeToString(hash[:])
 }
 
 func start(w http.ResponseWriter, r *http.Request) {
@@ -60,6 +75,8 @@ func start(w http.ResponseWriter, r *http.Request) {
 	values.Add("state", oauth.state)
 	values.Add("scope", oauth.scope)
 	values.Add("redirect_uri", oauth.clientRedirectUri)
+	values.Add("code_challenge_method", oauth.codeChallengeMethod)
+	values.Add("code_challenge", oauth.codeChallenge)
 
 	http.Redirect(w, r, authEndPoint+"?"+values.Encode(), http.StatusFound)
 }
@@ -72,7 +89,7 @@ func callback(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	body, err := apiRequest(r, res["accsess_token"].(string))
+	body, err := apiRequest(r, res["access_token"].(string))
 	if err != nil {
 		log.Println(err)
 	}
@@ -87,7 +104,56 @@ func tokenRequest(q url.Values) (map[string]interface{}, error) {
 	values.Add("client_id", oauth.clientId)
 	values.Add("client_secret", oauth.clientSecret)
 	values.Add("grantType", grantType)
+	values.Add("code", q.Get("code"))
+	values.Add("redirect_uri", oauth.clientRedirectUri)
 
+	req, err := http.NewRequest("POST", tokenEndPoint, strings.NewReader(values.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("token response : %s", string(body))
+	var data map[string]interface{}
+	json.Unmarshal(body, &data)
+	return data, nil
+}
+
+func apiRequest(r *http.Request, token string) ([]byte, error) {
+
+	photoAPI := "https://photoslibrary.googleapis.com/v1/mediaItems"
+
+	r, err := http.NewRequest("GET", photoAPI, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	r.Header.Set("Authorization", fmt.Sprintf("Bearer "+token))
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil || resp.StatusCode != 200 {
+		log.Printf("http status code is %d, err: %s", resp.StatusCode, err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return body, nil
 }
 
 //OAuth2Client is a client
@@ -97,7 +163,7 @@ func main() {
 
 	http.HandleFunc("/start", start)
 	http.HandleFunc("/callback", callback)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
